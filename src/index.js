e2b9b3525cc6db2fc72a0bf0d7cf5a4b4d7887ff
Andrey Shutov <tailsghost@vk.com>
2022-09-03 14:52:02 +0300

    Исправление ошибок и обрез больших строк

--- src/index.js
+++ src/index.js
@@ -1,18 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/app/App";
-import MarvelService from "./services/MarvelService";
+import ReactDOM from "react-dom/client";
 import "./style/style.scss";
+import App from "./components/app/App";
 
-const marvelService = new MarvelService();
-
-marvelService
-  .getAllCharacters()
-  .then((res) => res.data.results.forEach((item) => console.log(item.name)));
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
