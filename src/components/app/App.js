de47c6ccd560e0a1b2e294996b2634a889cc0e91
Andrey Shutov <tailsghost@vk.com>
2022-09-04 15:09:19 +0300

    Добавление предохранителя

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,6 +4,7 @@ import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
 import { Component } from "react";
 import decoration from "../../resources/img/vision.png";
+import ErrorBoundary from "../ErrorBoundary/ErrorBoundary";
 
 class App extends Component {
   state = {
@@ -21,10 +22,16 @@ class App extends Component {
       <div className="app">
         <AppHeader />
         <main>
-          <RandomChar />
+          <ErrorBoundary>
+            <RandomChar />
+          </ErrorBoundary>
           <div className="char__content">
-            <CharList onCharSelected={this.onCharSelected} />
-            <CharInfo charId={this.state.selectedChar} />
+            <ErrorBoundary>
+              <CharList onCharSelected={this.onCharSelected} />
+            </ErrorBoundary>
+            <ErrorBoundary>
+              <CharInfo charId={this.state.selectedChar} />
+            </ErrorBoundary>
           </div>
           <img
             className="bg-decoration"
