c873e2f2ed0e4324bfb5c40572234192d7cd4daa
Andrey Shutov <tailsghost@vk.com>
2022-09-05 16:52:32 +0300

    Добавление focus для карточек

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -5,6 +5,7 @@ import MarvelService from "../../services/MarvelService";
 import "./charList.scss";
 import propTypes from "prop-types";
 import CharInfo from "../charInfo/CharInfo";
+import React from "react";
 
 class CharList extends Component {
   state = {
@@ -16,11 +17,11 @@ class CharList extends Component {
     charEndend: false,
   };
 
-  marvelService = new MarvelService();
-
   componentDidMount() {
     this.onRequest();
   }
+
+  marvelService = new MarvelService();
 
   onRequest = (offset) => {
     this.onCharListLoading();
@@ -57,8 +58,23 @@ class CharList extends Component {
     });
   };
 
+  itemRefs = [];
+
+  setRef = (ref) => {
+    this.itemRefs.push(ref);
+  };
+
+  focusOnItem = (id) => {
+    console.log(id);
+    this.itemRefs.forEach((item) =>
+      item.classList.remove("char__item_selected")
+    );
+    this.itemRefs[id].classList.add("char__item_selected");
+    this.itemRefs[id].focus();
+  };
+
   renderItems(arr) {
-    const items = arr.map((item) => {
+    const items = arr.map((item, i) => {
       let imgStyle = { objectFit: "cover" };
       if (
         item.thumbnail ===
@@ -71,7 +87,18 @@ class CharList extends Component {
         <li
           className="char__item"
           key={item.id}
-          onClick={() => this.props.onCharSelected(item.id)}
+          tabIndex={0}
+          ref={this.setRef}
+          onClick={() => {
+            this.props.onCharSelected(item.id);
+            this.focusOnItem(i);
+          }}
+          onKeyPress={(e) => {
+            if (e.key === " " || e.key === "Enter") {
+              this.props.onCharSelected(item.id);
+              this.focusOnItem(i);
+            }
+          }}
         >
           <img
             src={item.thumbnail}
